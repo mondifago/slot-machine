d932c93446f0c09dc0948e73a4cb334966c84f09
ugochukwumaduakor <umaduakor@hotmail.com>
2024-03-10 19:36:25 +1100

    slot machine v1.0 populate and display grid

---
+++ slot_machine/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace slot_machine;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<char> listOfChars = new List<char>(){'a','b','c',};
+        char[,] slotMachine = new char[3,3];
+
+        for (int i = 0; i <3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Random rng = new Random();
+                int randomIndex = rng.Next(0, listOfChars.Count);
+                slotMachine[0, 0] = listOfChars[randomIndex];
+                slotMachine[0, 1] = listOfChars[randomIndex];
+                slotMachine[0, 2] = listOfChars[randomIndex];
+                slotMachine[1, 0] = listOfChars[randomIndex];
+                slotMachine[1, 1] = listOfChars[randomIndex];
+                slotMachine[1, 2] = listOfChars[randomIndex];
+                slotMachine[2, 0] = listOfChars[randomIndex];
+                slotMachine[2, 1] = listOfChars[randomIndex];
+                slotMachine[2, 2] = listOfChars[randomIndex];
+
+                //Console.WriteLine($"Element at position ({i},{j}): {slotMachine[i, j]}");
+
+                Console.Write(slotMachine[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+
