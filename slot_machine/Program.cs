17895f596ec508fcba139577add5dca88a0845c5
ugochukwumaduakor <umaduakor@hotmail.com>
2024-03-20 21:04:36 +1100

    added options 3 & 4

--- slot_machine/Program.cs
+++ slot_machine/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Xml.Linq;
@@ -19,12 +19,16 @@ class Program
         int amountWon = 0;
         int amountDeposited;
 
+        
+
+
+
         Console.WriteLine("*********************************|Welcome to Slot Machine|*********************************\n");
-        Console.WriteLine($"Option 1: - Pay ${SINGLE_LINE_COST} to play for all rows and win $2 for each line that matches");
-        Console.WriteLine($"Option 2: - Pay ${SINGLE_LINE_COST} to play for all columns and win $2 for each line that matches");
-        Console.WriteLine($"Option 3: - Pay ${DOUBLE_LINE_COST} to play for all rows and two diagonals and win $2 for each line that matches");
-        Console.WriteLine($"Option 4: - Pay ${DOUBLE_LINE_COST} to play for all columns and two diagonals and win $2 for each line that matches");
-        Console.WriteLine($"Option 5: - Pay ${ALL_LINE_COST} to play for any line and win ${WIN_AMOUNT} for each line that matches, and $100 Jackpot if all rows and columns matches\n");
+        Console.WriteLine($"Option 1: - Pay ${SINGLE_LINE_COST} to play for all rows and win ${WIN_AMOUNT} for each line that matches");
+        Console.WriteLine($"Option 2: - Pay ${SINGLE_LINE_COST} to play for all columns and win ${WIN_AMOUNT} for each line that matches");
+        Console.WriteLine($"Option 3: - Pay ${DOUBLE_LINE_COST} to play for all rows and two diagonals and win ${WIN_AMOUNT} for each line that matches");
+        Console.WriteLine($"Option 4: - Pay ${DOUBLE_LINE_COST} to play for all columns and two diagonals and win ${WIN_AMOUNT} for each line that matches");
+        Console.WriteLine($"Option 5: - Pay ${ALL_LINE_COST} to play for any line and win ${WIN_AMOUNT} for each line that matches, and ${JACKPOT_WIN} Jackpot if all rows and columns matches\n");
 
         Console.Write("Please choose the option you want to play and press ENTER:\t");
         int cashDepositSelection = Convert.ToInt32(Console.ReadLine());
@@ -57,14 +61,28 @@ class Program
             Console.WriteLine("\n");
         }
 
+        Random rng = new Random();
         List<char> listOfChars = new List<char>(){'a','b','c',};
         char[,] slotMachine = new char[3,3];
+
+
+        
+
+        
+
+
+
+
+
+
+
+
 
         for (int i = 0; i <3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                Random rng = new Random();
+                
                 int randomIndex = rng.Next(0, listOfChars.Count);
                 slotMachine[i, j] = listOfChars[randomIndex];
                 //Console.WriteLine($"Element at position ({i},{j}): {slotMachine[i, j]}");
@@ -76,6 +94,17 @@ class Program
         Console.WriteLine("Press ENTER to see amount won...");
         Console.WriteLine("\n");
         Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
 
         if (cashDepositSelection == 1)
         {
@@ -160,23 +189,155 @@ class Program
             Console.WriteLine("Total Amount won so far = $" + totalAmountWon);
         }
 
-            if (cashDepositSelection == 3)
+        if (cashDepositSelection == 3)
+        {
+            char center = slotMachine[1, 1];
+            char upRight = slotMachine[0, 2];
+            char downLeft = slotMachine[2, 0];
+            char downRight = slotMachine[2, 2];
+            char upLeft = slotMachine[0, 0];
+            char upMiddle = slotMachine[0, 1];
+            char downMiddle = slotMachine[2, 1];
+            char leftMiddle = slotMachine[1, 0];
+            char rightMiddle = slotMachine[1, 2];
+
+            bool diagonal1Same = upLeft == center && downRight == center;
+            bool diagonal2Same = upRight == center && downLeft == center;
+            bool row1Same = upLeft == upMiddle && upMiddle == upRight;
+            bool row2Same = leftMiddle == center && center == rightMiddle;
+            bool row3Same = downLeft == downMiddle && downMiddle == downRight;
+
+
+            // Print the grid
+            for (int i = 0; i < 3; i++)
             {
+                for (int j = 0; j < 3; j++)
+                {
+                    bool highlight = false;
+                    // Check if current cell is part of diagonal1 or diagonal2
+                    if ((i == j && i == 1) || (i == j && i == 0) || (i == j && i == 2))
+                    {
+                        if (diagonal1Same)
+                        {
+                            highlight = true;
+                        }
+                    }
+                    if ((i == j && i == 1) || (i == 0 && j == 2) || (i == 2 && j == 0))
+                    {
+                        if (diagonal2Same)
+                        {
+                            highlight = true;
+                        }
+                    }
+                    // Check if current cell is part of row1, row2, or row3
+                    if ((i == 0 && row1Same) || (i == 1 && row2Same) || (i == 2 && row3Same))
+                    {
+                        highlight = true;
+                    }
+                    if (highlight)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.Write(slotMachine[i, j] + " ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+            if (row1Same) { amountWon += WIN_AMOUNT; }
+            if (row2Same) { amountWon += WIN_AMOUNT; }
+            if (row3Same) { amountWon += WIN_AMOUNT; }
+            if (diagonal1Same) { amountWon += WIN_AMOUNT; }
+            if (diagonal2Same) { amountWon += WIN_AMOUNT; }
 
+            Console.WriteLine("you won $" + amountWon);
+            totalAmountWon += amountWon;
+            Console.WriteLine("Total Amount won so far = $" + totalAmountWon);
+        }
 
+        if (cashDepositSelection == 4)
+        {
+            char center = slotMachine[1, 1];
+            char upRight = slotMachine[0, 2];
+            char downLeft = slotMachine[2, 0];
+            char downRight = slotMachine[2, 2];
+            char upLeft = slotMachine[0, 0];
+            char upMiddle = slotMachine[0, 1];
+            char downMiddle = slotMachine[2, 1];
+            char leftMiddle = slotMachine[1, 0];
+            char rightMiddle = slotMachine[1, 2];
+
+            bool column1Same = slotMachine[0, 0] == slotMachine[1, 0] && slotMachine[1, 0] == slotMachine[2, 0];
+            bool column2Same = slotMachine[0, 1] == slotMachine[1, 1] && slotMachine[1, 1] == slotMachine[2, 1];
+            bool column3Same = slotMachine[0, 2] == slotMachine[1, 2] && slotMachine[1, 2] == slotMachine[2, 2];
+
+            bool diagonal1Same = upLeft == center && downRight == center;
+            bool diagonal2Same = upRight == center && downLeft == center;
+
+            // Print the grid
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    bool highlight = false;
+
+                    // Check if current cell is part of diagonal1 or diagonal2
+                    if ((i == j && i == 1) || (i == j && i == 0) || (i == j && i == 2))
+                    {
+                        if (diagonal1Same)
+                        {
+                            highlight = true;
+                        }
+                    }
+
+                    if ((i == j && i == 1) || (i == 0 && j == 2) || (i == 2 && j == 0))
+                    {
+                        if (diagonal2Same)
+                        {
+                            highlight = true;
+                        }
+                    }
+
+                    // Check if current cell is part of column1, column2, or column3
+                    if ((j == 0 && column1Same) || (j == 1 && column2Same) || (j == 2 && column3Same))
+                    {
+                        highlight = true;
+                    }
+
+                    if (highlight)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                    Console.Write(slotMachine[i, j] + " ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
             }
 
-            if (cashDepositSelection == 4)
-            {
+            if (column1Same) { amountWon += WIN_AMOUNT; }
+            if (column2Same) { amountWon += WIN_AMOUNT; }
+            if (column3Same) { amountWon += WIN_AMOUNT; }
+            if (diagonal1Same) { amountWon += WIN_AMOUNT; }
+            if (diagonal2Same) { amountWon += WIN_AMOUNT; }
+
+            Console.WriteLine("you won $" + amountWon);
+            totalAmountWon += amountWon;
+            Console.WriteLine("Total Amount won so far = $" + totalAmountWon);
+        }
 
 
-            }
-
-            if (cashDepositSelection == 5)
-            {
 
 
-            }
+
+
+
+
+
+        if (cashDepositSelection == 5)
+        {
+
+
+        }
 
 
         
