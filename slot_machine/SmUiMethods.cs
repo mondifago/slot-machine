85eea74afabbda310449505e342f6b0e6ed0645f
ugochukwumaduakor <umaduakor@hotmail.com>
2024-06-10 22:37:29 +1000

    Refactor Grid display with highlighted uniform lines

--- slot_machine/SmUiMethods.cs
+++ slot_machine/SmUiMethods.cs
@@ -3,6 +3,8 @@ namespace slot_machine
 {
 	public static class SmUiMethods
 	{
+        public static readonly Random rng = new Random();
+
         public static void DisplayWelcomeMessage()
         {
             Console.WriteLine("*********************************|Welcome to Slot Machine|*********************************\n");
@@ -94,7 +96,7 @@ namespace slot_machine
             {
                 for (int j = 0; j < SmConstants.GRID_COLUMN_DIM; j++)
                 {
-                    int randomIndex = SmConstants.RANDOM.Next(listOfChars.Count);
+                    int randomIndex = rng.Next(listOfChars.Count);
                     slotMachine[i, j] = listOfChars[randomIndex];
                     Console.Write(slotMachine[i, j] + " ");
                 }
@@ -117,6 +119,288 @@ namespace slot_machine
                     Console.WriteLine("\nInvalid key pressed. Please press Enter to continue.");
                 }
             }
+        }
+
+        public static void PrintGridHighlightingUniformLinesBasedOnModeSelected(int cashDepositSelection, char[,] slotMachine)
+        {
+            if (cashDepositSelection == SmConstants.CHECK_ROW_MODE)
+            {
+                bool[] uniformRows = Enumerable.Range(0, slotMachine.GetLength(0))
+                                      .Select(i => RowIsUniform(slotMachine, i))
+                                      .ToArray();
+
+                PrintOptionOneWithHighlighting(slotMachine, uniformRows);
+            }
+
+            if (cashDepositSelection == SmConstants.CHECK_COLUMN_MODE)
+            {
+                bool[] uniformColumns = Enumerable.Range(0, slotMachine.GetLength(1))
+                                           .Select(j => ColumnIsUniform(slotMachine, j))
+                                           .ToArray();
+
+                PrintOptionTwoWithHighlighting(slotMachine, uniformColumns);
+            }
+
+            if (cashDepositSelection == SmConstants.CHECK_ROW_AND_DIAGONAL_MODE)
+            {
+                bool[] uniformRows = Enumerable.Range(0, slotMachine.GetLength(0))
+                                      .Select(i => RowIsUniform(slotMachine, i))
+                                      .ToArray();
+                bool isLeftDiagonalUniform = CheckDiagonalUniform(slotMachine, true);
+                bool isRightDiagonalUniform = CheckDiagonalUniform(slotMachine, false);
+
+                PrintOptionThreeWithHighlighting(slotMachine, uniformRows, isLeftDiagonalUniform, isRightDiagonalUniform);
+            }
+
+            if (cashDepositSelection == SmConstants.CHECK_COLUMN_AND_DIAGONAL_MODE)
+            {
+                bool[] uniformColumns = Enumerable.Range(0, slotMachine.GetLength(1))
+                                           .Select(j => ColumnIsUniform(slotMachine, j))
+                                           .ToArray();
+                bool isLeftDiagonalUniform = CheckDiagonalUniform(slotMachine, true);
+                bool isRightDiagonalUniform = CheckDiagonalUniform(slotMachine, false);
+
+                PrintOptionFourWithHighlighting(slotMachine, uniformColumns, isLeftDiagonalUniform, isRightDiagonalUniform);
+            }
+
+            if (cashDepositSelection == SmConstants.CHECK_ALL_LINE_MODE)
+            {
+                bool[] uniformRows = Enumerable.Range(0, slotMachine.GetLength(0))
+                                      .Select(i => RowIsUniform(slotMachine, i))
+                                      .ToArray();
+                bool[] uniformColumns = Enumerable.Range(0, slotMachine.GetLength(1))
+                                           .Select(j => ColumnIsUniform(slotMachine, j))
+                                           .ToArray();
+                bool isLeftDiagonalUniform = CheckDiagonalUniform(slotMachine, true);
+                bool isRightDiagonalUniform = CheckDiagonalUniform(slotMachine, false);
+
+                PrintOptionFiveWithHighlighting(slotMachine, uniformRows, uniformColumns, isLeftDiagonalUniform, isRightDiagonalUniform);
+            }
+        }
+
+        public static void PrintOptionOneWithHighlighting(char[,] slotMachine, bool[] uniformRows)
+        {
+            for (int i = 0; i < slotMachine.GetLength(0); i++)
+            {
+                for (int j = 0; j < slotMachine.GetLength(1); j++)
+                {
+                    if (uniformRows[i])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                    }
+                    Console.Write(slotMachine[i, j] + " ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintOptionTwoWithHighlighting(char[,] slotMachine, bool[] uniformColumns)
+        {
+            for (int i = 0; i < slotMachine.GetLength(0); i++)
+            {
+                for (int j = 0; j < slotMachine.GetLength(1); j++)
+                {
+                    if (uniformColumns[j])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.Write(slotMachine[i, j] + " ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintOptionThreeWithHighlighting(char[,] slotMachine, bool[] uniformRows, bool isLeftDiagonalUniform, bool isRightDiagonalUniform)
+        {
+            for (int i = 0; i < slotMachine.GetLength(0); i++)
+            {
+                for (int j = 0; j < slotMachine.GetLength(1); j++)
+                {
+                    bool isDiagonal = (i == j && isLeftDiagonalUniform) || (i == slotMachine.GetLength(1) - 1 - j && isRightDiagonalUniform);
+                    if (uniformRows[i] || isDiagonal)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.Write(slotMachine[i, j] + " ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintOptionFourWithHighlighting(char[,] slotMachine, bool[] uniformColumns, bool isLeftDiagonalUniform, bool isRightDiagonalUniform)
+        {
+            for (int i = 0; i < slotMachine.GetLength(0); i++)
+            {
+                for (int j = 0; j < slotMachine.GetLength(1); j++)
+                {
+                    bool isDiagonal = (i == j && isLeftDiagonalUniform) || (i == slotMachine.GetLength(1) - 1 - j && isRightDiagonalUniform);
+                    if (uniformColumns[j] || isDiagonal)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.Write(slotMachine[i, j] + " ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintOptionFiveWithHighlighting(char[,] slotMachine, bool[] uniformRows, bool[] uniformColumns, bool isLeftDiagonalUniform, bool isRightDiagonalUniform)
+        {
+            for (int i = 0; i < slotMachine.GetLength(0); i++)
+            {
+                for (int j = 0; j < slotMachine.GetLength(1); j++)
+                {
+                    bool isRowUniform = uniformRows[i];
+                    bool isColumnUniform = uniformColumns[j];
+                    bool isDiagonal = (i == j && isLeftDiagonalUniform) || (i == slotMachine.GetLength(1) - 1 - j && isRightDiagonalUniform);
+                    if (isRowUniform || isColumnUniform || isDiagonal)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.Write(slotMachine[i, j] + " ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static bool RowIsUniform(char[,] slotMachine, int rowIndex)
+        {
+            char[] rowElements = Enumerable.Range(0, slotMachine.GetLength(1))
+                                           .Select(j => slotMachine[rowIndex, j])
+                                           .ToArray();
+            return rowElements.Distinct().Count() == 1;
+        }
+
+        public static bool ColumnIsUniform(char[,] slotMachine, int columnIndex)
+        {
+            char[] columnElements = Enumerable.Range(0, slotMachine.GetLength(0))
+                                              .Select(i => slotMachine[i, columnIndex])
+                                              .ToArray();
+            return columnElements.Distinct().Count() == 1;
+        }
+
+        public static bool CheckDiagonalUniform(char[,] slotMachine, bool leftDiagonal)
+        {
+            char[] diagonalElements = Enumerable.Range(0, slotMachine.GetLength(0))
+                                                .Select(i => leftDiagonal ? slotMachine[i, i] : slotMachine[i, slotMachine.GetLength(1) - 1 - i])
+                                                .ToArray();
+            return diagonalElements.Distinct().Count() == 1;
+        }
+
+        public static int CheckRowWin(char[,] slotMachine)
+        {
+            int amountWon = 0;
+            for (int i = 0; i < slotMachine.GetLength(0); i++)
+            {
+                if (RowIsUniform(slotMachine, i))
+                    amountWon += SmConstants.WIN_AMOUNT;
+            }
+            return amountWon;
+        }
+
+        public static int CheckColumnWin(char[,] slotMachine)
+        {
+            int amountWon = 0;
+            for (int j = 0; j < slotMachine.GetLength(1); j++)
+            {
+                if (ColumnIsUniform(slotMachine, j))
+                    amountWon += SmConstants.WIN_AMOUNT;
+            }
+            return amountWon;
+        }
+
+        public static int CheckRowOrDiagonalWin(char[,] slotMachine)
+        {
+            int amountWon = 0;
+            for (int i = 0; i < slotMachine.GetLength(0); i++)
+            {
+                if (RowIsUniform(slotMachine, i))
+                    amountWon += SmConstants.WIN_AMOUNT;
+            }
+            if (CheckDiagonalUniform(slotMachine, true))
+                amountWon += SmConstants.WIN_AMOUNT;
+            if (CheckDiagonalUniform(slotMachine, false))
+                amountWon += SmConstants.WIN_AMOUNT;
+            return amountWon;
+        }
+
+        public static int CheckColumnOrDiagonalWin(char[,] slotMachine)
+        {
+            int amountWon = 0;
+            for (int j = 0; j < slotMachine.GetLength(1); j++)
+            {
+                if (ColumnIsUniform(slotMachine, j))
+                    amountWon += SmConstants.WIN_AMOUNT;
+            }
+            if (CheckDiagonalUniform(slotMachine, true))
+                amountWon += SmConstants.WIN_AMOUNT;
+            if (CheckDiagonalUniform(slotMachine, false))
+                amountWon += SmConstants.WIN_AMOUNT;
+            return amountWon;
+        }
+
+        public static int CheckRowOrColumnDiagonalWin(char[,] slotMachine, bool[] uniformRows, bool[] uniformColumns)
+        {
+            int amountWon = 0;
+            bool jackpotWon = false;
+
+            if (uniformRows.All(x => x) && uniformColumns.All(x => x))
+            {
+                amountWon = SmConstants.JACKPOT_WIN;
+                jackpotWon = true;
+            }
+
+            if (!jackpotWon)
+            {
+                for (int i = 0; i < slotMachine.GetLength(0); i++)
+                {
+                    if (RowIsUniform(slotMachine, i))
+                        amountWon += SmConstants.WIN_AMOUNT;
+                }
+                for (int j = 0; j < slotMachine.GetLength(1); j++)
+                {
+                    if (ColumnIsUniform(slotMachine, j))
+                        amountWon += SmConstants.WIN_AMOUNT;
+                }
+                if (CheckDiagonalUniform(slotMachine, true))
+                    amountWon += SmConstants.WIN_AMOUNT;
+                if (CheckDiagonalUniform(slotMachine, false))
+                    amountWon += SmConstants.WIN_AMOUNT;
+            }
+
+            return amountWon;
+        }
+
+        public static int CalculateWinnings(int cashDepositSelection, char[,] slotMachine)
+        {
+            int amountWon = 0;
+            bool[] uniformRows = new bool[slotMachine.GetLength(0)];
+            bool[] uniformColumns = new bool[slotMachine.GetLength(1)];
+            switch (cashDepositSelection)
+            {
+                case SmConstants.CHECK_ROW_MODE:
+                    amountWon = CheckRowWin(slotMachine);
+                    break;
+                case SmConstants.CHECK_COLUMN_MODE:
+                    amountWon = CheckColumnWin(slotMachine);
+                    break;
+                case SmConstants.CHECK_ROW_AND_DIAGONAL_MODE:
+                    amountWon = CheckRowOrDiagonalWin(slotMachine);
+                    break;
+                case SmConstants.CHECK_COLUMN_AND_DIAGONAL_MODE:
+                    amountWon = CheckColumnOrDiagonalWin(slotMachine);
+                    break;
+                case SmConstants.CHECK_ALL_LINE_MODE:
+                    amountWon = CheckRowOrColumnDiagonalWin(slotMachine, uniformRows, uniformColumns);
+                    break;
+            }
+            return amountWon;
         }
     }
 }
