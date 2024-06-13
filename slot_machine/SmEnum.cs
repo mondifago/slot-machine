de04e3f175e560ece85fb5054536c4c64ba596d8
ugochukwumaduakor <umaduakor@hotmail.com>
2024-06-13 22:51:45 +1000

    create Enum file

---
+++ slot_machine/SmEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace slot_machine
+{
+    public enum GameMode
+    {
+        CHECK_ROW_MODE = 1,
+        CHECK_COLUMN_MODE = 2,
+        CHECK_ROW_AND_DIAGONAL_MODE = 3,
+        CHECK_COLUMN_AND_DIAGONAL_MODE = 4,
+        CHECK_ALL_LINE_MODE = 5
+    }
+}
+
